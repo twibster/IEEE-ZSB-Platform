5faeecee656c6918e40d44df76e420ff0e1523f0
Omar Omran <omaromran2003@gmail.com>
2023-05-09 09:07:33 +0300

    add tests for login endpoint and team data logic in register

--- backend/tests/test_routes.py
+++ backend/tests/test_routes.py
@@ -3,6 +3,10 @@ from fastapi import status
 from utils import generate_random_string
 
 BASE_URL = "http://localhost:8000/"
+USER_EMAIL = "teser@gmail.com"
+USER_USERNAME = "lobalizer"
+USER_PASSWORD = "tester123"
+
 
 def test_home():
     response = httpx.get(BASE_URL)
@@ -33,14 +37,15 @@ def test_user_already_exists():
         "first_name": "John",
         "last_name": "Jacky",
         "birthdate": "2023-04-24T22:01:32.904Z",
-        "email": generate_random_string(8, email=True),
-        "username": generate_random_string(8),
-        "password": "stringst",
+        "email": USER_EMAIL,
+        "username": USER_USERNAME,
+        "password": USER_PASSWORD,
         "chapter": "ras",
         "department": "embedded systems",
         "position": "leader"
     }
     response = httpx.post(BASE_URL+"register", json=body)
+    assert response.status_code == status.HTTP_201_CREATED
     response = httpx.post(BASE_URL+"register", json=body)
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
     assert response.json().get("detail") == [
@@ -54,4 +59,69 @@ def test_user_already_exists():
          }]
 
 
-    
+def test_team_data_logic():
+    body = {
+        "first_name": "John",
+        "last_name": "Jacky",
+        "birthdate": "2023-04-24T22:01:32.904Z",
+        "email": generate_random_string(8, email=True),
+        "username": generate_random_string(8),
+        "password": "stringst",
+        "chapter": "ras",
+        "department": "embedded systems",
+        "position": "chairman"
+    }
+    response = httpx.post(BASE_URL+"register", json=body)
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    body.pop("chapter")
+    response = httpx.post(BASE_URL+"register", json=body)
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    body["position"] = "leader"
+    response = httpx.post(BASE_URL+"register", json=body)
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    body.pop("department")
+    response = httpx.post(BASE_URL+"register", json=body)
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    body["position"] = "member"
+    response = httpx.post(BASE_URL+"register", json=body)
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    body["department"] = "ai"
+    response = httpx.post(BASE_URL+"register", json=body)
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    body["position"] = "rookie"
+    response = httpx.post(BASE_URL+"register", json=body)
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    body["chapter"] = "ras"
+    response = httpx.post(BASE_URL+"register", json=body)
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    body.pop("department")
+    response = httpx.post(BASE_URL+"register", json=body)
+    assert response.status_code == status.HTTP_201_CREATED
+
+
+def test_login_not_found():
+    body = {
+        "username": generate_random_string(8),
+        "password": generate_random_string(8)
+    }
+    response = httpx.post(BASE_URL+"login", data=body, headers={'Content-Type': 'application/x-www-form-urlencoded'})
+    assert response.json() == {'detail': 'user not found'}
+
+
+def test_login_incorrect_password():
+    body = {
+        "username": USER_EMAIL,
+        "password": generate_random_string(8)
+    }
+    response = httpx.post(BASE_URL+"login", data=body, headers={'Content-Type': 'application/x-www-form-urlencoded'})
+    assert response.json() == {'detail': 'Incorrect Password'}
+
+
+def test_login_get_token():
+    body = {
+        "username": USER_EMAIL,
+        "password": USER_PASSWORD
+    }
+    response = httpx.post(BASE_URL+"login", data=body, headers={'Content-Type': 'application/x-www-form-urlencoded'})
+    assert response.json().get("access_token") != None
+    assert response.json().get("token_type") == "bearer"
