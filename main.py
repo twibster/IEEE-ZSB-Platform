9fdca1ddea2cf8a804d9d47de6626dfbf3a86f0b
Omar Omran <omaromran2003@gmail.com>
2023-05-10 18:32:28 +0300

    remove startup time calculator in main

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-
-start = time.time()
-
 from backend.routes import app
 
 if __name__ == "__main__":
     import uvicorn
-    print(f"Startup took {time.time()-start} seconds")
     uvicorn.run("main:app", reload=True)
