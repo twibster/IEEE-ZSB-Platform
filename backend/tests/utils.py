20e59cbe9ce205b5b8632e735b69bd1389d6dba2
Omar Omran <omaromran2003@gmail.com>
2023-05-10 18:33:18 +0300

    add database table dropper and creator functions in test utils

--- backend/tests/utils.py
+++ backend/tests/utils.py
@@ -1,5 +1,8 @@
 import string
 import random
+from backend import engine
+from sqlalchemy.orm import sessionmaker
+
 
 def generate_random_string(length: int, email: bool = False) -> str:
     alpha = list(string.ascii_lowercase)
@@ -8,4 +11,17 @@ def generate_random_string(length: int, email: bool = False) -> str:
         generated_string += alpha[random.randint(0, len(alpha)-1)]
     return generated_string+"@default.com" if email else generated_string
 
-    
+
+def drop_database_tables(Base) -> None:
+    # Get the metadata object associated with the session's bind
+    metadata = Base.metadata
+
+    # Drop all tables
+    metadata.drop_all(bind=engine)
+
+
+def create_database_tables(Base,engine):
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    db = Session()
+    return db
